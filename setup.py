23e5f937936c38ba8228b2f8818ed0d9b4e9c2af
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2023-03-18 20:12:17 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='firebird',
+    version='1.0.1',
+    author='Jono Nicholas',
+    description='Graphing of monitored events',
+    packages=find_packages(),
+    install_requires=[
+        'websockets'
+    ]
+)
