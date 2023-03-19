72b12cd3817cb502f23b497f7b0b97d430c6c446
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2023-03-19 21:01:49 +0000

    Updated manifest

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='firebird',
-    version='1.0.7',
+    version='1.0.8',
     author='Jono Nicholas',
     description='Graphing of monitored events',
     packages=find_packages(exclude=['connect4']),
