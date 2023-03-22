747c7f62b42fece98c8f4e8e85487b46629ee3f4
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2023-03-22 23:27:03 +0000

    Added watcher logging

--- firebird/comms.py
+++ firebird/comms.py
@@ -4,22 +4,33 @@ from firebird import SESSIONS
 from firebird.watcher import monitor_logs
 import os
 import json
+import logging
+import logging.config
 
 
 __all__  = [
     'main'
 ]
 PORT = os.getenv('INTERNAL_WS_PORT', 8001)
+INSTANCE_PATH = os.getenv('INSTANCE_PATH')
+
+
+def get_logger(config_path=None):
+    config_path = config_path or os.path.join(INSTANCE_PATH, 'logging.conf')
+    logging.config.fileConfig(config_path)
+    return logging.getLogger('watcher')
 
 
 async def handler(websocket):   
     SESSIONS.add(websocket)
     update_connection_count()
+    logger.info(f'Session added: {websocket}')
     try:
         await websocket.wait_closed()
     finally:
         SESSIONS.remove(websocket)
         update_connection_count()
+        logger.info(f'Session removed: {websocket}')
 
 def update_connection_count():
     websockets.broadcast(SESSIONS, json.dumps({'connections': len(SESSIONS)}))
@@ -39,4 +50,6 @@ async def main():
 
 
 def run():
+    global logger 
+    logger = get_logger()
     asyncio.run(main())
